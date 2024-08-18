b2fd952f3a211dc55b45804d30c1a0ecf141ea68
Brahim Kedjar <98288166+brahimkedjar@users.noreply.github.com>
2024-08-18 22:51:38 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
-from flask import Flask
 from telegram import Update
-from telegram.ext import Application, CommandHandler, CallbackContext, Updater
+from telegram.ext import Application, CommandHandler, CallbackContext
 import logging
 
 # Replace 'YOUR_API_TOKEN' with your actual API token from BotFather
@@ -9,13 +8,8 @@ API_TOKEN = '7226265761:AAFT3jZ2a6sGRHZekSC3g5uBp5GZHX6a8UU'
 # Initialize logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-app = Flask(__name__)
-
-# Initialize the Updater and pass it your bot's token.
-updater = Updater(token=API_TOKEN)
-
-# Get the dispatcher to register handlers
-dispatcher = updater.dispatcher
+# Create an Application instance with the bot token
+application = Application.builder().token(API_TOKEN).build()
 
 async def start(update: Update, context: CallbackContext) -> None:
     # Create a button that launches the React.js web app
@@ -27,12 +21,8 @@ async def start(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text('Click the button below to open the game UI:', reply_markup=reply_markup)
 
 # Add a handler for the /start command
-dispatcher.add_handler(CommandHandler("start", start))
-
-def run_bot():
-    # Start the Bot
-    updater.start_polling()
+application.add_handler(CommandHandler("start", start))
 
 if __name__ == '__main__':
-    run_bot()
-    app.run(host='0.0.0.0', port=8080)
+    # Start polling for updates
+    application.run_polling()
